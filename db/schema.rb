3b5c5c87fffc782d15137f7926b58e7d7a197114
justgarcia5 <justgarcia5@gmail.comgit config --global user.email justgarcia5@gmail.com>
2019-07-30 15:12:30 -0700

    changed db from sqlite to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,13 +12,16 @@
 
 ActiveRecord::Schema.define(version: 2019_07_13_014858) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "contacts", force: :cascade do |t|
     t.string "name"
     t.string "phone"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
+    t.bigint "user_id"
     t.index ["user_id"], name: "index_contacts_on_user_id"
   end
 
@@ -36,4 +39,5 @@ ActiveRecord::Schema.define(version: 2019_07_13_014858) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "contacts", "users"
 end
